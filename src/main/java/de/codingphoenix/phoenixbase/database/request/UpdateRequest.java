4248df7c2c1c4285ce33a124a06b9e0c3dacf8dc
CodingPhoenixx <jan.f.meinl@web.de>
2024-10-18 08:33:27 +0200

    Added Dev Comments

--- src/main/java/de/codingphoenix/phoenixbase/database/request/UpdateRequest.java
+++ src/main/java/de/codingphoenix/phoenixbase/database/request/UpdateRequest.java
@@ -2,5 +2,26 @@ package de.codingphoenix.phoenixbase.database.request;
 
 public class UpdateRequest {
 
-//TODO
+//TODO:
+
+//    UPDATE table_name
+//    SET column1 = value1, column2 = value2, ...
+//    WHERE condition;
+//
+//    UPDATE table_name
+//    SET column_name = new_value;
+//
+//    UPDATE table_name
+//    SET column_name = new_value
+//    WHERE condition
+//    LIMIT number;
+//
+//    UPDATE IGNORE table_name
+//    SET column_name = new_value
+//    WHERE condition;
+//
+//    UPDATE LOW_PRIORITY table_name
+//    SET column_name = new_value
+//    WHERE condition;
+
 }
